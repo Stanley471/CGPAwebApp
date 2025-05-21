c8140451c9d095427ccd88a8b9238ea0c8418b06
Stanley Obimma <126078627+Stanley471@users.noreply.github.com>
2025-05-21 14:29:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var name = document.getElementById("name");
+document.getElementById("78").innerHTML = name;
+
+function startForm(){
+    document.getElementById("intro_text").style.display = "none";
+    document.getElementById("form").style.display = "block";
+}
